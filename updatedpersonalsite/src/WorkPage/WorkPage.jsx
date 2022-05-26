f71f50b1d45efae1a88fbdf32789af9d78b84908
Jack Barrett <jbarrett1347@gmail.com>
2022-05-26 11:44:16 -0500

    styled header

--- updatedpersonalsite/src/WorkPage/WorkPage.jsx
+++ updatedpersonalsite/src/WorkPage/WorkPage.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function WorkPage() {
   return (
     <div id="" class="bg-dark h-screen w-screen">
-      <div class="bg-lighter">
-        <p>what have I done?</p>
+      <div class="p-5 grid justify-items-center">
+        <div />
+        <div id="header" class="p-5 bg-lighter text-center">
+          <Link to="/Home">
+            <h1 class="text-8xl">what have I done?</h1>
+          </Link>
+        </div>
+        <div />
       </div>
     </div>
   );
