89695d8e8573eea60023aff277c18458a803f2b3
Jack Barrett <jbarrett1347@gmail.com>
2022-05-27 16:30:36 -0500

    made the landingpage boxes focusable & have the same changes made on focus

--- updatedpersonalsite/src/landingPage/landingPage.jsx
+++ updatedpersonalsite/src/landingPage/landingPage.jsx
@@ -1,3 +1,4 @@
+// importing react because it's awesome
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -5,6 +6,8 @@ export default function LandingPage() {
   function useHover() {
     const [hovering, setHovering] = useState(false);
     const onHoverProps = {
+      onBlur: () => setHovering(false),
+      onFocus: () => setHovering(true),
       onMouseEnter: () => setHovering(true),
       onMouseLeave: () => setHovering(false),
     };
@@ -18,34 +21,79 @@ export default function LandingPage() {
   return (
     <div
       id="mainContentContainer"
-      class="p-5 grid gap-y-20 h-screen w-screen bg-darker md:gap-x-8 text-center md:grid-cols-5 md:content-center"
+      class="
+      p-5
+      grid 
+      w-screen
+      h-screen
+      gap-y-20
+      bg-darker
+      md:gap-x-8
+      text-center
+      md:grid-cols-5
+      md:content-center
+      "
     >
       <div />
 
-      <div {...heyHoverProps} class="bg-main p-4 hover:bg-light cursor-pointer">
-        <p class="text-7xl font-Cairo">
-          {heyHover ? <Link to="/ContactMe">contact</Link> : "hey!"}
-        </p>
-      </div>
-
-      <div {...imHoverProps} class="bg-main p-4 hover:bg-light cursor-pointer ">
-        <p class="text-7xl font-Cairo">
-          {imHover ? <Link to="/MyWork">my work</Link> : "I'm"}
-        </p>
-      </div>
-
-      <div
-        {...jackHoverProps}
-        class="bg-main p-4 hover:bg-light cursor-pointer"
+      <button
+        class="
+        p-4
+        bg-main
+        text-7xl
+        truncate
+        font-Cairo
+        cursor-pointer
+        hover:bg-light
+        focus:bg-light
+        "
+        {...heyHoverProps}
       >
-        <p class="text-7xl font-Cairo">
-          {jackHover ? <Link to="/AboutMe">about</Link> : "Jack"}
-        </p>
-      </div>
+        {heyHover ? <Link to="/ContactMe">contact</Link> : "hey!"}
+      </button>
+
+      <button
+        class="
+        p-4
+        bg-main
+        text-7xl
+        truncate
+        font-Cairo
+        cursor-pointer
+        hover:bg-light
+        focus:bg-light
+        "
+        {...imHoverProps}
+      >
+        {imHover ? <Link to="/MyWork">my work</Link> : "I'm"}
+      </button>
+
+      <button
+        class="
+        p-4
+        bg-main
+        text-7xl
+        truncate
+        font-Cairo
+        cursor-pointer
+        hover:bg-light
+        focus:bg-light
+        "
+        {...jackHoverProps}
+      >
+        {jackHover ? <Link to="/AboutMe">about</Link> : "Jack"}
+      </button>
+
       <div />
 
       <div>
-        <p class="text-white sm:opacity-0 font-Cairo">
+        <p
+          class="
+        text-white
+        font-Cairo
+        sm:opacity-0
+        "
+        >
           tap on a card to get started :)
         </p>
       </div>
