d472308389531222c08aae09de12849a96efbc6b
Jack Barrett <jbarrett1347@gmail.com>
2022-06-01 18:28:55 -0500

    added better styling to the contact page, and made mobile friendly

--- updatedpersonalsite/src/ContactPage/ContactPage.jsx
+++ updatedpersonalsite/src/ContactPage/ContactPage.jsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-// import ContactForm from "./ContactForm";
 
 export default function ContactPage() {
   return (
-    <div id="contactContainer" class="bg-dark w-screen h-screen">
-      {/*  */}
+    <div id="contactContainer" class="bg-dark w-full h-full">
       <div class="p-5 grid justify-items-center">
         <div id="header" class="p-5 bg-lighter text-center">
           <Link to="/Home">
@@ -13,38 +11,63 @@ export default function ContactPage() {
         </div>
       </div>
 
-      <div
-        class="
-        h-96
-        w-2/3
-        text-4xl
-        bg-light
-        text-center
-        hover:bg-darker
-        hover:text-white"
-      >
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/jack-barrett02/"
-        >
-          <p>LinkedIn</p>
-        </a>
-      </div>
+      <div class="py-2 md:py-10 grid grid-cols-5 gap-y-4">
+        <div class="text-4xl bg-light col-end-5 text-center col-start-2 hover:bg-darker hover:text-white">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Thirteenhelens"
+          >
+            <div class="py-20 md:py-52">
+              <p>GitHub</p>
+              <svg
+                fill="none"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="h-6 w-6 inline-flex"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
+            </div>
+          </a>
+        </div>
 
-      <br />
-      <div class="bg-light h-96 text-center">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/Thirteenhelens"
-        >
-          GitHub
-        </a>
+        <div class="text-4xl bg-light col-end-5 text-center col-start-2 hover:bg-darker hover:text-white">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/jack-barrett02/"
+          >
+            <div class="py-20 md:py-52">
+              <p>LinkedIn</p>
+              <svg
+                fill="none"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="h-6 w-6 inline-flex"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
+            </div>
+          </a>
+        </div>
+
+        <div class="text-4xl bg-light col-end-5 text-center col-start-2 hover:bg-darker hover:text-white">
+          <p class="py-20 md:py-56">Email me</p>
+        </div>
       </div>
-      <br />
-      <div class="bg-light h-96 text-center">Email me</div>
-      {/* <ContactForm /> */}
     </div>
   );
 }
