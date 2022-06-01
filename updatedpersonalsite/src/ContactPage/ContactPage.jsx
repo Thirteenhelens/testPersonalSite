4c64dea4ccc6ae95a97ed083afbcea724f4e9856
Jack Barrett <jbarrett1347@gmail.com>
2022-06-01 15:25:33 -0500

    working onlayout :)

--- updatedpersonalsite/src/ContactPage/ContactPage.jsx
+++ updatedpersonalsite/src/ContactPage/ContactPage.jsx
@@ -1,48 +1,49 @@
 import { Link } from "react-router-dom";
-import ContactForm from "./ContactForm";
+// import ContactForm from "./ContactForm";
 
 export default function ContactPage() {
   return (
     <div id="contactContainer" class="bg-dark w-screen h-screen">
+      {/*  */}
       <div class="p-5 grid justify-items-center">
-        <div />
         <div id="header" class="p-5 bg-lighter text-center">
           <Link to="/Home">
             <h1 class="text-5xl md:text-8xl">get in touch with me</h1>
           </Link>
         </div>
-        <div />
-
-        <div id="socials" class="bg-light text-center py-2 px-60 mt-5">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/jack-barrett02/"
-          >
-            {" "}
-            LinkedIn |
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/Thirteenhelens"
-          >
-            {" "}
-            GitHub |
-          </a>
-          <a href="mailto:jbarrett1347@gmail.com"> Email</a>
-        </div>
       </div>
 
-<div class="bg-light h-96">
+      <div
+        class="
+        h-96
+        w-2/3
+        text-4xl
+        bg-light
+        text-center
+        hover:bg-darker
+        hover:text-white"
+      >
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/jack-barrett02/"
+        >
+          <p>LinkedIn</p>
+        </a>
+      </div>
 
-</div>
-
-<div>
-
-</div>
-
-
+      <br />
+      <div class="bg-light h-96 text-center">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Thirteenhelens"
+        >
+          GitHub
+        </a>
+      </div>
+      <br />
+      <div class="bg-light h-96 text-center">Email me</div>
       {/* <ContactForm /> */}
     </div>
   );
