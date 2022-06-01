f1c5b34018b101a624aaed21023026aa4b6601c5
Jack Barrett <jbarrett1347@gmail.com>
2022-06-01 18:59:14 -0500

    accedintally made the landing page much less accsessable, but now you can click on any part of the box and be sent to the right component

--- updatedpersonalsite/src/landingPage/landingPage.jsx
+++ updatedpersonalsite/src/landingPage/landingPage.jsx
@@ -1,8 +1,10 @@
 // importing react because it's awesome
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
   function useHover() {
     const [hovering, setHovering] = useState(false);
     const onHoverProps = {
@@ -21,66 +23,37 @@ export default function LandingPage() {
   return (
     <div
       id="mainContentContainer"
-      class="
-      p-5
-      grid 
-      w-screen
-      h-screen
-      gap-y-20
-      bg-darker
-      md:gap-x-8
-      text-center
-      md:grid-cols-5
-      md:content-center
-      "
+      class="p-5 grid  w-screen h-screen gap-y-20 bg-darker md:gap-x-8 text-center md:grid-cols-5 md:content-center"
     >
       {/* purposely empty so the 3 boxes are in the center */}
       <div />
 
       <button
-        class="
-        p-4
-        bg-main
-        text-7xl
-        truncate
-        font-Cairo
-        cursor-pointer
-        hover:bg-light
-        focus:bg-light
-        "
+        class="p-4 bg-main text-7xl truncate font-Cairo cursor-pointer hover:bg-light focus:bg-light"
         {...heyHoverProps}
+        onClick={() => {
+          navigate("/ContactMe");
+        }}
       >
         {heyHover ? <Link to="/ContactMe">contact</Link> : "hey!"}
       </button>
 
       <button
-        class="
-        p-4
-        bg-main
-        text-7xl
-        truncate
-        font-Cairo
-        cursor-pointer
-        hover:bg-light
-        focus:bg-light
-        "
+        class="p-4 bg-main text-7xl truncate font-Cairo cursor-pointer hover:bg-light focus:bg-light"
         {...imHoverProps}
+        onClick={() => {
+          navigate("/MyWork");
+        }}
       >
         {imHover ? <Link to="/MyWork">my work</Link> : "I'm"}
       </button>
 
       <button
-        class="
-        p-4
-        bg-main
-        text-7xl
-        truncate
-        font-Cairo
-        cursor-pointer
-        hover:bg-light
-        focus:bg-light
-        "
+        class="p-4 bg-main text-7xl truncate font-Cairo cursor-pointer hover:bg-light focus:bg-light"
         {...jackHoverProps}
+        onClick={() => {
+          navigate("/AboutMe");
+        }}
       >
         {jackHover ? <Link to="/AboutMe">about</Link> : "Jack"}
       </button>
@@ -88,13 +61,7 @@ export default function LandingPage() {
       <div />
 
       <div>
-        <p
-          class="
-        text-white
-        font-Cairo
-        sm:opacity-0
-        "
-        >
+        <p class="text-white font-Cairo sm:opacity-0">
           tap on a card to get started :)
         </p>
       </div>
